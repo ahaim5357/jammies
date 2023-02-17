36f8797d5da646fc5b252c6d16b9758c8751eaae
Aaron Haim <ahaim5357@gmail.com>
2023-02-17 16:02:49 -0500

    Document patcher

--- src/project_patcher/workspace/patcher.py
+++ src/project_patcher/workspace/patcher.py
@@ -1,53 +1,133 @@
+"""TODO: Document"""
+
 # License: Public domain (CC0)
 # Isaac Turner 2016/12/05
 
 import re
 from difflib import unified_diff
-from typing import Iterator, List
+from typing import Iterator, List, Optional
+from datetime import datetime
 
 _NO_EOL: str = '\\ No newline at end of file'
 """Text indicating there is no newline at the end of the diff file."""
 
-_HUNK_HEADER: str = r'^@@ -(\d+),?(\d+)? \+(\d+),?(\d+)? @@$'
+_HUNK_HEADER: re.Pattern[str] = re.compile(r'^@@ -(\d+),?(\d+)? \+(\d+),?(\d+)? @@$')
 """The regex to get the file line information from the hunk header."""
 
+class PatchError(ValueError):
+    """The patch file contained invalid information.
+    """
+
 def create_patch(from_text: str, to_text: str, filename: str = '') -> str:
-    """TODO: Document
+    """Creates a patch between two pieces of text. If equivalent, returns
+    an empty string.
+
+    Parameters
+    ----------
+    from_text : str
+        The original text to generate the patch from.
+    to_text : str
+        The new text the patch will transform the original text to.
+    filename : str (default '')
+        The name of the file the patch is being applied for.
+
+    Returns
+    -------
+    str
+        The generated patch.
     """
 
     diffs: Iterator[str] = unified_diff(
         from_text.splitlines(keepends = True),
         to_text.splitlines(keepends = True),
-        fromfile = filename, tofile = filename
+        fromfile = filename, tofile = filename,
+        tofiledate = str(datetime.now())
     )
     # Join diffs together
+    # Apply no eol if text line doesn't end with a newline
     return ''.join([diff if diff[-1] == '\n' else f'{diff}\n{_NO_EOL}\n' for diff in diffs])
 
 def apply_patch(text: str, patch: str, revert: bool = False) -> str:
-    """TODO: Document
+    """Applies a patch to the current text.
+
+    Parameters
+    ----------
+    text : str
+        The text to apply the patch to.
+    patch : str
+        The patch to apply.
+    revert : bool (default False)
+        When `True`, will attempt to recover the original file.
+
+    Returns
+    -------
+    str
+        The patched text.
     """
+
+    # Setup basic references and result vars
     text: List[str] = text.splitlines(keepends = True)
     patch: List[str] = patch.splitlines(keepends = True)
-    patched_file: str = ''
+    patched_text: str = ''
 
-    # TODO: Rewrite below
-    i = sl = 0
-    (midx,sign) = (1,'+') if not revert else (3,'-')
-    while i < len(patch) and patch[i].startswith(("---","+++")): i += 1 # skip header lines
-    while i < len(patch):
-        m = re.match(_HUNK_HEADER, patch[i])
-        if not m: raise Exception("Bad patch -- regex mismatch [line "+str(i)+"]")
-        l = int(m.group(midx))-1 + (m.group(midx+1) == '0')
-        if sl > l or l > len(text):
-            raise Exception("Bad patch -- bad line num [line "+str(i)+"]")
-        patched_file += ''.join(text[sl:l])
-        sl = l
-        i += 1
-        while i < len(patch) and patch[i][0] != '@':
-            if i+1 < len(patch) and patch[i+1][0] == '\\': line = patch[i][:-1]; i += 2
-            else: line = patch[i]; i += 1
+    pidx: int = 0 # Index line into patch
+    tidx: int = 0 # Index line into text
+
+    # Determine whether to apply changes or revert them
+    ## midx is the index into the header to determine what to apply
+    ## sign is whether to consider the addition or removal as the final transformer
+    midx, sign = (1,'+') if not revert else (3,'-') # midx, sign: int, str
+
+    # Skip header lines
+    while pidx < len(patch) and patch[pidx].startswith(("---","+++")):
+        pidx += 1
+
+    # Apply patches as long as there are still patch lines left
+    while pidx < len(patch):
+        # Get header
+        header: Optional[re.Match[str]] = _HUNK_HEADER.match(patch[pidx])
+        if not header: # If there is no hunk header, throw an exception
+            raise PatchError(f'No header found for new hunk on line {pidx}')
+
+        # Get the first line index in the text file to apply/revert the patch for
+        shidx = int(header.group(midx)) - 1 + (header.group(midx + 1) == '0')
+        # Make sure start hunk index is not after current text index
+        ## and that the start hunk index is not after the number of lines in the text file
+        if tidx > shidx:
+            raise PatchError(f'Hunk start index {shidx} is after the current text index {tidx}')
+        if shidx > len(text):
+            raise PatchError(f'Hunk start index {shidx} is after the end of the text')
+
+        # Add any lines before the hunk start index to the result
+        patched_text += ''.join(text[tidx:shidx])
+
+        # Set the new text start index and move to next patch line
+        tidx = shidx
+        pidx += 1
+
+        # Loop through patch until eol or next hunk
+        while pidx < len(patch) and patch[pidx][0] != '@':
+            # If the next patch line is not the end of the text
+            ## and the first character isn't a backslash
+            ### (indicates either \t, \n, or the _NO_EOL text)
+            if pidx + 1 < len(patch) and patch[pidx + 1][0] == '\\':
+                # Get the line without the last character
+                ## and increase the patch index by 2
+                line: str = patch[pidx][:-1]
+                pidx += 2
+            else:
+                # Otherwise, get the current line
+                line: str = patch[pidx]
+                pidx += 1
+            # If the line isn't empty
             if len(line) > 0:
-                if line[0] == sign or line[0] == ' ': patched_file += line[1:]
-                sl += (line[0] != sign)
-    patched_file += ''.join(text[sl:])
-    return patched_file
+                # If the first character of the line is the sign
+                ## or a space
+                if line[0] == sign or line[0] == ' ':
+                    # Apply the patched text without the first character
+                    patched_text += line[1:]
+                # Skip the line if it is not the current sign being checked for
+                tidx += (line[0] != sign)
+    # Apply the rest of the text after all patches were made
+    patched_text += ''.join(text[tidx:])
+    return patched_text
