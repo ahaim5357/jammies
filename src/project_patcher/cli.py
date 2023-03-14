debb7f8a2973a96e2796d2ea9eb65c80249a7d1e
Aaron Haim <ahaim5357@gmail.com>
2023-03-14 12:40:00 -0400

    [Feature] Add new cli (#12)
    * Add new cli
    
    - Cleanup some pylint things
    
    * Update README
    
    * Remove TODOs
    
    * Fix bad json

--- src/project_patcher/cli.py
+++ src/project_patcher/cli.py
@@ -13,11 +13,6 @@ def main() -> None:
     implementation.
     """
 
-# TODO: Create clean workspace
-# TODO: Generate working directory
-# TODO: Generate patches
-# TODO: Push to output directory
-
 @main.command(name = 'init')
 @click.option(
     '--import_metadata', '-I',
@@ -43,7 +38,7 @@ def init(import_metadata: Optional[str] = None, include_hidden: bool = False) ->
     wspc.setup_clean(metadata)
     wspc.setup_working(include_hidden = include_hidden)
 
-    print('Success!')
+    print('Initialized patching environment!')
 
 @main.command(name = 'output')
 def output() -> None:
@@ -56,4 +51,47 @@ def output() -> None:
     # Output working and generate patches
     wspc.output_working(metadata)
 
-    print('Success!')
+    print('Generated patches and output files!')
+
+@main.command(name = 'clean')
+@click.option(
+    '--import_metadata', '-I',
+    type = str,
+    default = None,
+    help = 'A path or URL to the metadata JSON.'
+)
+def clean(import_metadata: Optional[str] = None) -> None:
+    """Initializes a clean workspace from the
+    metadata JSON in the executing directory, an import, or from
+    the metadata builder if neither are present.
+    """
+
+    # Get metadata
+    metadata: ProjectMetadata = wspc.read_metadata(import_loc = import_metadata)
+
+    # Setup workspace
+    wspc.setup_clean(metadata)
+
+    print('Setup clean workspace!')
+
+@main.command(name = 'src')
+@click.option(
+    '--import_metadata', '-I',
+    type = str,
+    default = None,
+    help = 'A path or URL to the metadata JSON.'
+)
+def source(import_metadata: Optional[str] = None) -> None:
+    """Initializes a patched workspace from the
+    metadata JSON in the executing directory, an import, or from
+    the metadata builder if neither are present.
+    """
+
+    # Get metadata
+    metadata: ProjectMetadata = wspc.read_metadata(import_loc = import_metadata)
+
+    # Setup workspace
+    wspc.setup_clean(metadata, '_src', invalidate_cache = True)
+    wspc.setup_working_raw()
+
+    print('Setup patched workspace!')
